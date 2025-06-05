e1d98e9c0df7a52395eeeee639bf4ea14e270f09
Jonathan <alayj3665@gmail.com>
2025-06-05 10:53:40 -0500

    Arreglo de urls.py

--- Aplicaciones/Reservacion/urls.py
+++ Aplicaciones/Reservacion/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('', views.turista),
     path('turista', views.turista),
     path('nuevoTurista', views.nuevoTurista),
     path('agregarTurista', views.agregarTurista),
