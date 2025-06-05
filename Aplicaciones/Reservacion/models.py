9d0b023897d656917596902151e5410f4414f868
Jonathan <alayj3665@gmail.com>
2025-06-04 20:37:10 -0500

    Agregue el modelo turista

--- Aplicaciones/Reservacion/models.py
+++ Aplicaciones/Reservacion/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+class Turista(models.Model):
+    id_turista = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20)
+    nacionalidad = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} ({self.nacionalidad})"
 # Create your models here.
